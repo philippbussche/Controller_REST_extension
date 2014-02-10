7f359ec418fb33a6be0a9b0d877f26dc8ea42564
Stefan Marx <smarx@marxworld.net>
2014-02-10 14:12:56 +0100

    Easier local Update of dev environment (requires adDevLab scripts and environmnet settings)

---
+++ updateController.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+gradle build
+echo "Require to be in ADDev Mode !!!"
+echo "--"
+echo "updating $CONTROLLER_DIR"
+echo ""
+cp rest_api/build/libs/ace_Rest_api-1.2-SNAPSHOT.jar $CONTROLLER_DIR/domains/domain1/applications/controller/controller-web_war/WEB-INF/lib
+cp rest_util/build/libs/ace_Rest_util-1.2-SNAPSHOT.jar $CONTROLLER_DIR/domains/domain1/applications/controller/controller-web_war/WEB-INF/lib
+
+ls -al $CONTROLLER_DIR/domains/domain1/applications/controller/controller-web_war/WEB-INF/lib/ace*.jar
+
